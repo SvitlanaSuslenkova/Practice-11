6ab5072ad00c2f54d9557977556bc8438364d5ea
SvitlanaSuslenkova <136314004+SvitlanaSuslenkova@users.noreply.github.com>
2023-12-14 18:48:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
+'use strict';
   let errorCircle = document.querySelector(".errorCircle");
   let textError = document.querySelector(".textError");
+  let input = document.querySelector(".emailarea");
 
-  function ValidateEmail(input) {
-
-    var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  function validateEmail(event) {
+    event.preventDefault();
+    let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   
     if (input.value.match(validRegex)) {
         errorCircle.style.display='none';
         textError.style.display='none';
-        document.form1.email.focus();
-        input.value="Your email was submitted!";
-  
-      return true;
+       input.value="Your email was submitted!";
+
   } 
   else {
         errorCircle.style.display='block';
         textError.style.display='block';
-        document.form1.email.focus();
-  
-      return false; 
+       document.form1.email.focus(); 
   }
   }
+
+  input.addEventListener ('input', function(){
+    errorCircle.style.display='none';
+    textError.style.display='none';
+  });
